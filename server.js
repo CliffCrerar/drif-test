d8192b34bf77aeac17fbd5611b09176a5f21569e
cliff-crerar <cliffc@agilebridge.co.za>
2020-05-24 15:08:14 +0200

    second commit

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express')
 const {createReadStream} = require('fs')
 const app = express()
 // app.use(express.static('public'));
-app.get('/',(req,res)=>createReadStream('./index.html').pipe(res.status(200)))
+app.get('/',(req,res)=>createReadStream('./public/index.html').pipe(res.status(200)))
 app.listen(3000);
 
